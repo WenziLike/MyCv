1039911196dd6dccbea48b2bfc5d6c320d338529
WenziLike <SlavaMyraxin@icloud.com>
2021-01-31 23:47:16 +0100

    Responsive

--- src/components/AppGeneralCv.vue
+++ src/components/AppGeneralCv.vue
@@ -8,35 +8,11 @@
             <div class="wrapper-logo">
               <strong>{{ logo }}</strong>
             </div>
-            <div class="card">
-              <img
-                src="./../assets/myfoto.jpg"
-                class="circle-image"
-                alt="Viacheslav Murakhin"
-              />
-            </div>
+            <div class="circle-image"></div>
             <ul class="card-contacts">
               <li v-for="item in contacts" :key="item">
                 <a :href="item.href">{{ item.text }}</a>
               </li>
-              <!-- <li>
-                <strong>tel:</strong>
-                <a href="tel:+48537886161"
-                  ><strong> +48 537 88 61 61</strong>
-                </a>
-              </li>
-              <li>
-                <strong>email:</strong>
-                <a href="mailto:SlavaMyraxin@icloud.com"
-                  ><strong> SlavaMyraxin@icloud.com</strong>
-                </a>
-              </li>
-              <li>
-                <strong>Skype:</strong>
-                <a href="callto:skype:live:.cid.f00786ab772de349"
-                  ><strong> Skype</strong>
-                </a>
-              </li> -->
             </ul>
             <div class="card-adress">
               <p>31-425 Kraków, Poland</p>
@@ -72,4 +48,38 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#app {
+  .btn {
+    position: absolute;
+    bottom: 6%;
+    right: 4%;
+    text-align: right;
+    font-size: 1rem;
+    font-weight: 600;
+    text-transform: uppercase;
+    height: 50px;
+    min-width: 120px;
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    background: #dde1e7;
+    color: #3498db;
+    box-shadow: 4px 5px 10px #707070b2, 2px 2px 5px rgba(94, 104, 121, 0.288);
+    &:active {
+      box-shadow: -3px -3px 7px #ffffff73, 2px 2px 5px rgba(94, 104, 121, 0.288);
+      color: #2c3e50;
+      text-align: left;
+    }
+  }
+  .circle-image {
+    width: 300px;
+    height: 300px; /* Ширина и высота */
+    border-radius: 50%; /* Превращаем в круг */
+    background: url("./../assets/myfoto.jpg") no-repeat -50px 0; /* Параметры фона */
+    background-size: auto 400px; /* Высота фотографии равна высоте элемента */
+    box-shadow: 2px 6px 12px rgba(0, 0, 0, 0.2);
+  }
+}
 </style>
